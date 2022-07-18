e0d5fb38732b8d4a64b01feadf988c5bfc452e7c
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:36:10 +0530

    crud-html-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./style.css";
+//crud - create[post],read[get],update[patch,put],delete[delete]
 
 export default function App() {
   return (
     <div>
+      insert form
+      updated form
+      all data 
+      table
+
 
     </div>
   );
