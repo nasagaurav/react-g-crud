5cf854a9942eb8a2da8f0592035f300d0ffa571d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:00:11 +0530

    addeed update form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import axios from 'axios';
 
 export default function App() {
   const [ob1, setob1] = useState({ name: '', email: '', gender: '' });
+  const [ob2, setob2] = useState({ id: '', name: '', email: '', gender: '' });
   const handleChange1 = (e) => {
-    console.log(e.target.placeholder, e.target.value);
+    console.log('ob1', e.target.placeholder, e.target.value);
+  };
+  const handleChange2 = (e) => {
+    console.log('ob2', e.target.placeholder, e.target.value);
   };
   const _insert = () => {
+    const url = `https://example1-g-crud-default-rtdb.firebaseio.com/users/{-N7TT_k_BbSLdhfwszx6 ?},json`;
+  };
+
+  const _update = () => {
     const url =
       'https://example1-g-crud-default-rtdb.firebaseio.com/users.json';
   };
@@ -31,8 +39,27 @@ export default function App() {
         />
         <button onClick={_insert}>insert</button>
       </div>
-      <div>update form</div>
-      all data table
+      <div>
+        update form
+        <input
+          placeholder="name"
+          value={(ob2, name)}
+          onChange={handleChange2}
+        />
+        <input
+          placeholder="email"
+          value={(ob2, email)}
+          onChange={handleChange2}
+        />
+        <input
+          placeholder="gender"
+          value={(ob2, gender)}
+          onChange={handleChange2}
+        />
+        <button onClick={_update}>update</button>
+      </div>
+      <h3>all users</h3>
+      table
     </div>
   );
 }
